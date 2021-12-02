d95f59b88c4cfb342e9929e45a3d35e77e4811b8
tayyrov <atayyrov@gmail.com>
2021-12-02 12:16:44 +0100

    Adding all codes

---
+++ codes/day1_task1.py
@@ -0,0 +1,22 @@
+"""
+Advent Of Code 2021
+Day 1
+Date: 01-12-2021
+Site: https://adventofcode.com/2021/day/1
+"""
+
+import sys
+
+file1 = open('../input_files/day1_input1.txt', 'r')
+
+ans = 0
+nums = list(map(int, file1.readlines()))
+ln = len(nums)
+for i in range(1, ln):
+    if nums[i-1] < nums[i]:
+        ans += 1
+
+
+
+print(ans)
+
---
+++ codes/day1_task2.py
@@ -0,0 +1,24 @@
+"""
+Advent Of Code 2021
+Day 1
+Date: 01-12-2021
+Site: https://adventofcode.com/2021/day/1
+"""
+
+import sys
+
+file1 = open('../input_files/day1_input1.txt', 'r')
+
+ans = 0
+nums = list(map(int, file1.readlines()))
+ln = len(nums)
+for i in range(ln-3):
+    num1 = nums[i] + nums[i+1] + nums[i+2]
+    num2 = nums[i+1] + nums[i+2] + nums[i+3]
+    if num1 < num2:
+        ans += 1
+
+
+
+print(ans)
+
