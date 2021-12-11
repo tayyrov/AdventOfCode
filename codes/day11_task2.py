35cddad30de3af49f5297c6afc2b424c398a6d2f
tayyrov <atayyrov@gmail.com>
2021-12-11 12:09:05 +0100

    Solutions for day11 task1 and task2 are added

---
+++ codes/day11_task2.py
@@ -0,0 +1,57 @@
+"""
+Advent Of Code 2021
+Day 11
+Date: 11-12-2021
+Site: https://adventofcode.com/2021/day/11
+Author: Tayyrov
+"""
+
+
+def isValid(r, c):
+    return 0 <= c < cols and 0 <= r < rows
+
+def allFlashed(matrix):
+    return max(max(row) for row in matrix) == 0
+
+
+input_file = open('../input_files/day11_input', 'r')
+
+matrix = [list(map(int, list(line.strip()))) for line in input_file.readlines()]
+
+rows = len(matrix)
+cols = len(matrix[0])
+directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (-1, 1), (1, 1), (-1, -1)]
+
+steps = 0
+total_flashes = 0
+while True:
+    stack = []
+    flashed = set()
+    for r in range(rows):
+        for c in range(cols):
+            if matrix[r][c] == 9:
+                stack.append((r, c))
+                total_flashes += 1
+                flashed.add((r, c))
+                matrix[r][c] = 0
+            else:
+                matrix[r][c] += 1
+    while stack:
+        r, c = stack.pop()
+
+        for dx, dy in directions:
+            new_r, new_c = r + dx, c + dy
+
+            if isValid(new_r, new_c) and (new_r, new_c) not in flashed:
+                if matrix[new_r][new_c] == 9:
+                    total_flashes += 1
+                    flashed.add((new_r, new_c))
+                    matrix[new_r][new_c] = 0
+                    stack.append((new_r, new_c))
+                else:
+                    matrix[new_r][new_c] += 1
+    steps += 1
+
+    if allFlashed(matrix):
+        print(steps)
+        break
